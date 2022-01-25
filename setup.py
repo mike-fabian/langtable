2359d5445316fea3f5be8aed148126790164c867
Mike FABIAN <mfabian@redhat.com>
2022-01-25 08:25:43 +0100

    Release 0.0.57

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     # within the egg directory easily:
     zip_safe=False,
     name='langtable',
-    version='0.0.56',
+    version='0.0.57',
     packages=setuptools.find_packages(),
     description='guess reasonable defaults for locale, keyboard, territory, ...',
     long_description=codecs.open('README', encoding='UTF-8').read(),
